7bfd0e3c46cec2eda080f146d213756ea6d3b936
Arthur Tolley <arthurellistolley@gmail.com>
2026-02-04 14:51:15 +0000

    update api url

--- frontend/config.js
+++ frontend/config.js
@@ -3,11 +3,11 @@
 
 const CONFIG = {
     // Backend API URL - Cloudflare tunnel domain
-    API_BASE_URL: 'https://spotify-tracker-api.4298756.xyz',
-    
+    API_BASE_URL: 'https://spotify.4298756.xyz',
+
     // GitHub Pages URL
     FRONTEND_URL: 'https://arthurtolley.github.io/spotify-playlist-tracker',
-    
+
     // Application info
     APP_NAME: 'Spotify Playlist Tracker',
     VERSION: '1.0.0'
